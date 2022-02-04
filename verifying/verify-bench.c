508e2899eca7c98825091409d046603f993542ed
Cong <cong6264@gmail.com>
2022-02-03 21:27:06 -0500

    Update verify-bench.c

--- verifying/verify-bench.c
+++ verifying/verify-bench.c
@@ -449,7 +449,7 @@ int main(){
 
 	my_time = ((long long)(end.tv_sec - start.tv_sec))*1000000000 + (end.tv_nsec - start.tv_nsec);
 
-	printf("My verification time = %lld ns\n", ( (long long) my_time/(ITERATIONS*8) );
+	printf("My verification time = %lld ns\n", ((long long) my_time/(ITERATIONS*8)) );
 
 	return 0;
 
