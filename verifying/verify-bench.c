85ab9d6f3c75db2410979a6798718a04c0c87a87
Cong <cong6264@gmail.com>
2022-02-04 00:03:24 -0500

    Update verify-bench.c

--- verifying/verify-bench.c
+++ verifying/verify-bench.c
@@ -511,7 +511,7 @@ int main(int argc, char* argv[]){
 
 	//med = median(ITERATIONS,  my_time);  
 	
-	printf("My verification median = %lld ns, len =%d,  ITERATIONS=%d \n", ((long long) med/8), len, ITERATIONS);
+	printf("My verification median = %lld ns, len =%d,  ITERATIONS=%d \n", ((long long) med), len, ITERATIONS);
 
 	return 0;
 
