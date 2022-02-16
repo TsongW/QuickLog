2dceeb54a516f3db402fcc49f0095a528f594d49
Cong <cong6264@gmail.com>
2022-02-16 15:43:22 -0500

    Create quick_test.sh

---
+++ signing/quick_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+make clean && make
+
+sudo dmesg --clear
+sudo insmod quickmod.ko
+dmesg
+sudo rmmod quickmod
+
+
+sudo dmesg --clear
+sudo insmod quickmod.ko
+dmesg
+sudo rmmod quickmod
+
+sudo dmesg --clear
+sudo insmod quickmod.ko
+dmesg
+sudo rmmod quickmod
+
+sudo dmesg --clear
+sudo insmod quickmod.ko
+dmesg
+sudo rmmod quickmod
+
+
+sudo dmesg --clear
+sudo insmod quickmod.ko
+dmesg
+sudo rmmod quickmod
