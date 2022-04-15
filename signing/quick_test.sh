8a53a7133cf3be6edcae801c42dbf65f02de1343
Cong <cong6264@gmail.com>
2022-04-15 11:03:37 -0400

    Update quick_test.sh

--- signing/quick_test.sh
+++ signing/quick_test.sh
@@ -4,28 +4,35 @@
 #make clean && make
 
 sudo dmesg --clear
-sudo insmod quickmod.ko
+sudo insmod quickmod.ko len=[64]
 dmesg
 sudo rmmod quickmod
 
 
 sudo dmesg --clear
-sudo insmod quickmod.ko
+sudo insmod quickmod.ko len=[128]
 dmesg
 sudo rmmod quickmod
 
 sudo dmesg --clear
-sudo insmod quickmod.ko
+sudo insmod quickmod.ko len=[256]
 dmesg
 sudo rmmod quickmod
 
 sudo dmesg --clear
-sudo insmod quickmod.ko
+sudo insmod quickmod.ko len=[320]
 dmesg
 sudo rmmod quickmod
 
 
 sudo dmesg --clear
-sudo insmod quickmod.ko
+sudo insmod quickmod.ko len=[512]
 dmesg
 sudo rmmod quickmod
+
+
+sudo dmesg --clear
+sudo insmod quickmod.ko len=[1024]
+dmesg
+sudo rmmod quickmod
+
